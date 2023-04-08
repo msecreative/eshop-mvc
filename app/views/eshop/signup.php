f10997edae9706360a6cccc6a6b16f2aaeec3ec5
msecreative <47583511+msecreative@users.noreply.github.com>
2023-04-08 21:01:19 +0600

    Part 9

--- app/views/eshop/signup.php
+++ app/views/eshop/signup.php
@@ -3,7 +3,7 @@
 	<section id="form"><!--form-->
 		<div class="container">
 			<div class="row">
-				<div class="col-sm-4 col-sm-offset-4">
+				<div class="col-sm-4 col-sm-offset-1">
 					<div class="login-form"><!--login form-->
 						<h2>Login to your account</h2>
 						<form action="#">
@@ -15,9 +15,19 @@
 							</span>
 							<button type="submit" class="btn btn-default">Login</button>
 						</form>
-						<br>
-						<a href="<?=ROOT ?>signup">Don't have an account? Signup here</a>
 					</div><!--/login form-->
+				</div>
+			
+				<div class="col-sm-4">
+					<div class="signup-form"><!--sign up form-->
+						<h2>New User Signup!</h2>
+						<form action="#">
+							<input type="text" placeholder="Name"/>
+							<input type="email" placeholder="Email Address"/>
+							<input type="password" placeholder="Password"/>
+							<button type="submit" class="btn btn-default">Signup</button>
+						</form>
+					</div><!--/sign up form-->
 				</div>
 			</div>
 		</div>
