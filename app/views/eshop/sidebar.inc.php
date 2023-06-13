aa8df7c0e48323b707e554f3f6161a396efba06f
msecreative <47583511+msecreative@users.noreply.github.com>
2023-06-13 22:55:38 +0600

    Part 148

---
+++ app/views/eshop/sidebar.inc.php
@@ -0,0 +1,41 @@
+<div class="col-sm-3">
+					<div class="left-sidebar">
+						<h2>Category</h2>
+						<div class="panel-group category-products" id="accordian"><!--category-productsr-->
+
+							<!-- category with subcategory -->
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#sportswear">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Sportswear
+										</a>
+									</h4>
+								</div>
+								<div id="sportswear" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="">Nike </a></li>
+											<li><a href="">Under Armour </a></li>
+											<li><a href="">Adidas </a></li>
+											<li><a href="">Puma</a></li>
+											<li><a href="">ASICS </a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<!-- category without subcate -->
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title"><a href="#">Kids</a></h4>
+								</div>
+							</div>
+					
+						</div><!--/category-productsr-->
+						
+						<div class="shipping text-center"><!--shipping-->
+							<img src="<?=ASSETS . THEME?>images/home/shipping.jpg" alt="" />
+						</div><!--/shipping-->
+					</div>
+				</div>
