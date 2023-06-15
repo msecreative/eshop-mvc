ece66876a3dbc33089b1718b8dc9efef1c891762
msecreative <47583511+msecreative@users.noreply.github.com>
2023-06-15 20:28:59 +0600

    Part 154

--- app/views/eshop/admin/socials.php
+++ app/views/eshop/admin/socials.php
@@ -42,32 +42,35 @@
                 <div class="order-table">
                     <h4><i class="fa fa-angle-right"></i>Product List</h4>  
                     <hr>
-                    <table class="table table-striped table-advance table-hover table-class">
-                        <thead>
-                            <tr>
-                                <th>Serial</th>
-                                <th>Setting Name</th>
-                                <th>Setting Value</th>
-                                <th>Status</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody">
-                            <?php 
-                                $i = 0;
-                                foreach ($settings as $setting) {
-                                $i++;
-                            ?>
-                            <tr>
-                                <td><?=$i ?></td>
-                                <td><a href="<?=ROOT?>profile"><?=$setting->setting ?></a></td>
-                                <td><?=$setting->value ?></td>
-                                <td><span onclick="disable_row('.$args.')" class="label label-'.$status_color.' label-mini" style="cursor:pointer">'.$catRow->disabled.'</span></td>
-                                <td style="font-size: 10px;">view</td>
-                            </tr>
-                            <?php } ?>
-                        </tbody>
-                    </table>
+                    <form action="" method="POST">
+                        <table class="table table-striped table-advance table-hover table-class">
+                            <thead>
+                                <tr>
+                                    <th>Serial</th>
+                                    <th>Setting Name</th>
+                                    <th>Setting Value</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody">
+                                <?php 
+                                    $i = 0;
+                                    foreach ($settings as $setting) {
+                                    $i++;
+                                ?>
+                                <tr>
+                                    <td><?=$i ?></td>
+                                    <td><a href="<?=ROOT?>profile"><?=ucwords(str_replace("-", " ",$setting->setting)) ?></a></td>
+                                    <td><input type="text" class="form-control" name="value" value="<?=$setting->value ?>"></td>
+                                    <td><span onclick="disable_row('.$args.')" class="label label-'.$status_color.' label-mini" style="cursor:pointer">'.$catRow->disabled.'</span></td>
+                                    <td style="font-size: 10px;">view</td>
+                                </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                        <input type="submit" value="Save" class="btn btn-primary pull-right">
+                    </form>
                 </div>
                 <?php }else{ ?>
                     <div style="padding: 10px;">No social link found</div>
